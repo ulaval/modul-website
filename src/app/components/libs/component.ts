793ebe86d6975b965d4bde4bb8e24ad656eb126e
Martin Simard <martin.simard@dti.ulaval.ca>
2017-06-19 14:38:42 -0400

    Updated component view for attribute display

--- src/app/components/libs/component.ts
+++ src/app/components/libs/component.ts
@@ -3,10 +3,12 @@ import Component from 'vue-class-component';
 import { Watch } from 'vue-property-decorator';
 import WithRender from './component.html?style=./component.scss';
 import * as ModulActions from '@/app/store/actions';
-import { Overview, OverviewType } from 'modul-components/dist/meta/meta';
+import Meta, { ComponentMeta, ComponentAttribute, Overview, OverviewType } from 'modul-components/dist/meta/meta';
 import { ModulVue } from 'modul-components/dist/utils/vue';
 
 import { ServiceMixin } from 'modul-components/dist/services/component-meta-impl';
+
+const BOOLEAN_TYPE: string = 'boolean';
 
 @WithRender
 @Component({
@@ -31,6 +33,38 @@ export class ComponentViewer extends ModulVue {
 
     public getItemContent(item: Overview): string {
         return this.$i18n.translate(item.content);
+    }
+
+    public getAttributes(meta: ComponentMeta): string[] {
+        return Meta.getComponentAttributes(meta);
+    }
+
+    public getAttribute(tag: string): ComponentAttribute {
+        return this.$store.state.composantState.attributes[tag];
+    }
+
+    public getValues(attribute: ComponentAttribute): string[] {
+        if (attribute.type == BOOLEAN_TYPE) {
+            return ['true', 'false'];
+        } else {
+            return attribute.values;
+        }
+    }
+
+    public isDefault(attribute: ComponentAttribute, value: any): boolean {
+        if (attribute.default) {
+            return attribute.type == BOOLEAN_TYPE ? attribute.default.toString() == value : attribute.default == value;
+        } else {
+            return attribute.type == BOOLEAN_TYPE ? value == 'false' : attribute.default == value;
+        }
+    }
+
+    public getValuesCount(attribute: ComponentAttribute): number {
+        if (attribute.type == BOOLEAN_TYPE) {
+            return 2;
+        } else {
+            return attribute.values.length;
+        }
     }
 
     private isOverviewType(item: Overview, type: OverviewType): boolean {
