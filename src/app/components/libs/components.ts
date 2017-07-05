8d84809d0f6b8d86e7cd7581c80eab6378bf5405
Philippe Rail <philipperail@gmail.com>
2017-07-05 17:00:00 -0400

    Update child and side navigation, add dropdown navigation

--- src/app/components/libs/components.ts
+++ src/app/components/libs/components.ts
@@ -1,33 +1,33 @@
 import Vue from 'vue';
+import { ModulVue } from '@ulaval/modul-components/dist/utils/vue/vue';
 import Component from 'vue-class-component';
 import WithRender from './components.html?style=./components.scss';
 import * as ModulActions from '@/app/store/actions';
+import Meta, { ComponentMeta, ComponentAttribute, Overview, OverviewType } from '@ulaval/modul-components/dist/meta/meta';
 import { FRENCH } from '@ulaval/modul-components/dist/utils/i18n/i18n';
 
 @WithRender
 @Component
-export class Components extends Vue {
+export class Components extends ModulVue {
 
-    public get getComponentsName(): String[] {
+    public currentComponent: Object = this.$store.state.componentRoutes[0];
+    public isListOpen: boolean;
+    public zindex: Number;
 
-        let i;
-        let componentsName: String[] = [];
-
-        for (i = 0; i < this.$store.state.componentRoutes.length; i++) {
-            if (this.$store.state.componentRoutes[i].name != '') {
-                componentsName.push(this.$store.state.componentRoutes[i].name);
-            }
+    public getComponentsName(element: any): String {
+        let name: string = 'default'; // todo, remove when data will all be completed
+        if (element.element.name != '') {
+            name = this.$i18n.translate(element.element.name);
         }
-
-        return componentsName;
+        return name;
     }
 
     public getPreviusComponent() {
 
         let activePath = this.$route.path;
         let i: number = 0;
-        let index: number = 0;
         let max: number = this.$store.state.componentRoutes.length - 1;
+        let index: number = max;
         for (i; i < this.$store.state.componentRoutes.length; i++) {
             if (this.$store.state.componentRoutes[i].url == activePath) {
                 if (i != 0) {
@@ -37,8 +37,8 @@ export class Components extends Vue {
             }
         }
 
+        this.currentComponent = this.$store.state.componentRoutes[index];
         this.$router.push(this.$store.state.componentRoutes[index].url);
-
     }
 
     public getNextComponent() {
@@ -56,8 +56,24 @@ export class Components extends Vue {
             }
         }
 
+        this.currentComponent = this.$store.state.componentRoutes[index];
         this.$router.push(this.$store.state.componentRoutes[index].url);
+    }
 
+    public toComponent(route) {
+        if (route.url != '') {
+            this.currentComponent = route;
+            this.$router.push(route.url);
+        }
+    }
+
+    public toSelectedComponent(event) {
+        this.currentComponent = event;
+        this.$router.push(event.url);
+    }
+
+    public setZindex(index) {
+        console.log(index);
     }
 
     public get isPageComponent(): boolean {
@@ -68,7 +84,7 @@ export class Components extends Vue {
         } else {
             isPageComponent = false;
         }
-        console.log('isPageComponent:');
+
         return isPageComponent;
     }
 
