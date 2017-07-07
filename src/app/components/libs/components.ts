d8f9b596f4ee4776b736ddeb61ebad68e01b5f7f
Philippe Rail <philipperail@gmail.com>
2017-07-07 13:16:34 -0400

    Update navigation, cleaned up html & css, added some transitions...

--- src/app/components/libs/components.ts
+++ src/app/components/libs/components.ts
@@ -6,15 +6,23 @@ import * as ModulActions from '@/app/store/actions';
 import Meta, { ComponentMeta, ComponentAttribute, Overview, OverviewType } from '@ulaval/modul-components/dist/meta/meta';
 import { FRENCH } from '@ulaval/modul-components/dist/utils/i18n/i18n';
 
+const ZINDEX: Number = 200;
+
 @WithRender
 @Component
 export class Components extends ModulVue {
 
-    public currentComponent: Object = this.$store.state.componentRoutes[0];
-    public isListOpen: boolean;
-    public zindex: Number;
+    private currentComponent: Object = this.$store.state.componentRoutes[0];
+    private zIndex: Number = 0;
+    private isScrolling: Boolean = false;
+    private hasScrolled: Boolean = false;
+    private isListOpened: Boolean = false;
+    private scrollPosition: Number = 0;
+    private bodyElement: HTMLElement = document.body;
 
-    public getComponentsName(element: any): String {
+    private storeReady: boolean = this.$store.state.componentRoutes != undefined ? true : false;
+
+    private getComponentsName(element: any): String {
         let name: string = 'default'; // todo, remove when data will all be completed
         if (element.element.name != '') {
             name = this.$i18n.translate(element.element.name);
@@ -22,7 +30,19 @@ export class Components extends ModulVue {
         return name;
     }
 
-    public getPreviusComponent() {
+    private get isPageComponent(): Boolean {
+        let isPageComponent: boolean;
+
+        if (this.$route.path == '/composants') {
+            isPageComponent = true;
+        } else {
+            isPageComponent = false;
+        }
+
+        return isPageComponent;
+    }
+
+    private getPreviusComponent(): void {
 
         let activePath = this.$route.path;
         let i: number = 0;
@@ -41,7 +61,7 @@ export class Components extends ModulVue {
         this.$router.push(this.$store.state.componentRoutes[index].url);
     }
 
-    public getNextComponent() {
+    private getNextComponent(): void {
 
         let activePath = this.$route.path;
         let i: number = 0;
@@ -60,35 +80,73 @@ export class Components extends ModulVue {
         this.$router.push(this.$store.state.componentRoutes[index].url);
     }
 
-    public toComponent(route) {
+    private toComponent(route): void {
         if (route.url != '') {
             this.currentComponent = route;
             this.$router.push(route.url);
+            this.bodyElement.scrollTop = 0;
         }
     }
 
-    public toSelectedComponent(event) {
+    private toSelectedComponent(event): void {
         this.currentComponent = event;
         this.$router.push(event.url);
     }
 
-    public setZindex(index) {
-        console.log(index);
+    private setIsListOpened(isListOpened): void {
+        this.isListOpened = isListOpened;
+        // reset the scroll
+        this.hasScrolled = false;
     }
 
-    public get isPageComponent(): boolean {
-        let isPageComponent: boolean;
+    private get setZindex(): Number {
 
-        if (this.$route.path == '/composants') {
-            isPageComponent = true;
+        if (this.isListOpened && !this.hasScrolled) {
+            this.zIndex = ZINDEX;
         } else {
-            isPageComponent = false;
+            this.zIndex = 0;
         }
 
-        return isPageComponent;
+        return this.zIndex;
     }
 
-    public mounted(): void {
+    // private animDropDownOnScroll(scrollPosition): void {
+    //     let position = this.$refs.dropdown['scrollHeight'];
+    //     this.$refs.dropdown['style'].transform = 'translate3d(0,' + (position + scrollPosition) + ', 0)';
+    // }
+
+    // https://gomakethings.com/detecting-when-a-visitor-has-stopped-scrolling-with-vanilla-javascript/
+    private onScroll(): void {
+
+        let isScrolling;
+
+        // Clear our timeout throughout the scroll
+        window.clearTimeout(isScrolling);
+
+        this.isScrolling = true;
+        this.hasScrolled = true;
+        this.isListOpened = false;
+
+        // Set a timeout to run after scrolling ends
+        isScrolling = setTimeout(() => {
+
+            // Run the callback
+            this.isScrolling = false;
+            this.scrollPosition = this.bodyElement.scrollTop;
+            // console.log('Scrolling has stopped: ' + this.scrollPosition);
+
+        }, 66);
+
+    }
+
+    private mounted(): void {
         this.$store.dispatch(ModulActions.COMPONENTS_META_GET, FRENCH);
+        window.addEventListener('scroll', this.onScroll);
+        console.log(this.$store.state.componentRoutes);
+
+    }
+
+    private destroyed() {
+        window.removeEventListener('mousedown', this.onScroll);
     }
 }
