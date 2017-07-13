9d9fd06eb89cc1738a6cce4fe102fffdf9c27b7a
Philippe Rail <philipperail@gmail.com>
2017-07-13 15:37:06 -0400

    Merge, added menu and animations

--- src/app/components/modul/modul.ts
+++ src/app/components/modul/modul.ts
@@ -1,24 +1,68 @@
 import Vue from 'vue';
+import { ModulVue } from '@ulaval/modul-components/dist/utils/vue/vue';
 import Component from 'vue-class-component';
 import WithRender from './modul.html?style=./modul.scss';
 import { routes, COMPONENTS, ECOSYSTEM, VISUAL_STANDARDS, WRITING_RULES } from '@/app/router';
 
+// animation constant shared with css in header.scss and menu.scss
+const CSS_ANIMATION_HEADER_DURATION: Number = 500;
+const CSS_ANIMATION_MENU_DURATION: Number = 800;
+
 @WithRender
 @Component
-export default class Modul extends Vue {
-    public get visualStandards(): string {
+export default class Modul extends ModulVue {
+
+    private menuOpen: boolean = false;
+    private headerAnimationCompleted: boolean = false;
+
+    private get isHome(): boolean {
+        let isHome = false;
+        if (this.$route.path == '/') {
+            isHome = true;
+        }
+        return isHome;
+    }
+
+    private get visualStandards(): string {
         return '/' + routes[VISUAL_STANDARDS];
     }
 
-    public get writingRules(): string {
+    private get writingRules(): string {
         return '/' + routes[WRITING_RULES];
     }
 
-    public get components(): string {
+    private get components(): string {
         return '/' + routes[COMPONENTS];
     }
 
-    public get ecosystem(): string {
+    private get ecosystem(): string {
         return '/' + routes[ECOSYSTEM];
     }
+
+    private showMenu(): void {
+
+        this.headerAnimationCompleted = false;
+        let anim: any = undefined;
+
+        if (this.menuOpen) {
+
+            let anim = setTimeout(() => {
+                this.menuOpen = false;
+                this.$mWindow.activeScollBody();
+            }, CSS_ANIMATION_MENU_DURATION);
+
+        } else {
+
+            this.$mWindow.stopScollBody();
+            let anim = setTimeout(() => {
+                this.headerAnimationCompleted = true;
+            }, CSS_ANIMATION_HEADER_DURATION);
+
+            this.menuOpen = true;
+        }
+
+        clearTimeout(anim);
+
+    }
+
 }
