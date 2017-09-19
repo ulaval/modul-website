3548291adf18f2ef17d7d43e9371a27199531c4a
Raphaël Paré <raph.pare@outlook.com>
2017-09-19 17:48:43 -0400

    Update home page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,13 @@
 import { ModulVue } from '@ulaval/modul-components/dist/utils/vue/vue';
 import Component from 'vue-class-component';
 import WithRender from './home.html?style=./home.scss';
+import { MediaQueries, MediaQueriesMixin } from '@ulaval/modul-components/dist/mixins/media-queries/media-queries';
 import ElementQueries from 'css-element-queries/src/ElementQueries';
 
 @WithRender
-@Component
+@Component({
+    mixins: [MediaQueries]
+})
 export class HomePage extends ModulVue {
 
     private experimentTitlePosition: number = 1;
@@ -31,18 +34,20 @@ export class HomePage extends ModulVue {
     }
 
     private setParallaxEffect() {
-        let windowHeight: number = window.innerHeight;
-        let scrollY: number = this.$modul.scrollPosition == 0 ? this.$modul.stopScrollPosition : this.$modul.scrollPosition;
-        let designTop: number = (this.$refs.design as HTMLElement).getBoundingClientRect().top - 60;
-        let designTemplateMaxWidth: number = (this.$refs.designBody as HTMLElement).clientWidth;
-        let designTemplateWidthRemaining: number = designTemplateMaxWidth - this.designTemplateMinWidth;
-        this.experimentContentPosition = scrollY / 18;
-        this.experimentTitlePosition = - (scrollY / 18);
-        this.designButtonPosition = (designTop - 230) <= windowHeight ? ((designTop - 230) - windowHeight) / 2 : 1;
-        if (designTop >= 0) {
-            this.designTemplateWidth = designTop <= designTemplateWidthRemaining ? designTemplateMaxWidth - designTop : this.designTemplateMinWidth;
-        } else {
-            this.designTemplateWidth = designTemplateMaxWidth;
+        if (this.as<MediaQueriesMixin>().isMqMinS) {
+            let windowHeight: number = window.innerHeight;
+            let scrollY: number = this.$modul.scrollPosition == 0 ? this.$modul.stopScrollPosition : this.$modul.scrollPosition;
+            let designTop: number = (this.$refs.design as HTMLElement).getBoundingClientRect().top - 70;
+            let designTemplateMaxWidth: number = (this.$refs.designBody as HTMLElement).clientWidth;
+            let designTemplateWidthRemaining: number = designTemplateMaxWidth - this.designTemplateMinWidth;
+            this.experimentContentPosition = scrollY / 18;
+            this.experimentTitlePosition = - (scrollY / 18);
+            this.designButtonPosition = (designTop - 60) <= windowHeight ? ((designTop - 60) - windowHeight) / 2 : 1;
+            if (designTop >= 0) {
+                this.designTemplateWidth = designTop <= designTemplateWidthRemaining ? designTemplateMaxWidth - designTop : this.designTemplateMinWidth;
+            } else {
+                this.designTemplateWidth = designTemplateMaxWidth;
+            }
         }
     }
 }
