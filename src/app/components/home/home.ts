f25eb5ad12fe1361894b62fa0362c233ba6f6e3a
Raphaël Paré <raph.pare@outlook.com>
2017-09-19 17:24:51 -0400

    Update home page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -13,6 +13,7 @@ export class HomePage extends ModulVue {
     private designTemplateWidth: number = this.designTemplateMinWidth;
     private scrollDesignStart: boolean = false;
     private widthStep: number = 1;
+    private designButtonPosition: number = 1;
 
     protected mounted(): void {
         this.setParallaxEffect();
@@ -30,12 +31,14 @@ export class HomePage extends ModulVue {
     }
 
     private setParallaxEffect() {
+        let windowHeight: number = window.innerHeight;
         let scrollY: number = this.$modul.scrollPosition == 0 ? this.$modul.stopScrollPosition : this.$modul.scrollPosition;
         let designTop: number = (this.$refs.design as HTMLElement).getBoundingClientRect().top - 60;
         let designTemplateMaxWidth: number = (this.$refs.designBody as HTMLElement).clientWidth;
         let designTemplateWidthRemaining: number = designTemplateMaxWidth - this.designTemplateMinWidth;
         this.experimentContentPosition = scrollY / 18;
         this.experimentTitlePosition = - (scrollY / 18);
+        this.designButtonPosition = (designTop - 230) <= windowHeight ? ((designTop - 230) - windowHeight) / 2 : 1;
         if (designTop >= 0) {
             this.designTemplateWidth = designTop <= designTemplateWidthRemaining ? designTemplateMaxWidth - designTop : this.designTemplateMinWidth;
         } else {
