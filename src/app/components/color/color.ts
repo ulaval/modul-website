d583deff0e9ae8dc6e7d8e698eeab60fd36c6588
Raphaël Paré <raph.pare@outlook.com>
2018-01-16 22:13:53 -0500

    Update modul-color

--- src/app/components/color/color.ts
+++ src/app/components/color/color.ts
@@ -15,6 +15,8 @@ export class MColor extends Vue {
     private green: number;
     private blue: number;
 
+    private textColor: string = '#000';
+
     private get rgb(): string {
         let shorthandRegex: any = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
         let hex: string = this.hex.replace(shorthandRegex, (m, r, g, b) => {
@@ -27,35 +29,21 @@ export class MColor extends Vue {
         this.green = parseInt(result[2], 16);
         this.blue = parseInt(result[3], 16);
 
+        let yiq = ((this.red * 299) + (this.green * 587) + (this.blue * 114)) / 1000;
+        this.textColor = (yiq >= 120) ? '#000' : '#fff';
+
         return result ? this.red + ', ' + this.green + ', ' + this.blue : undefined;
     }
 
-    private get cmyk() {
-        // let cyan = 1 - this.red;
-        // let magenta = 1 - this.green;
-        // let yellow = 1 - this.blue;
-        // let black = 1;
+    private get cmyk(): string {
+        let r: number = this.red / 255;
+        let g: number = this.green / 255;
+        let b: number = this.blue / 255;
 
-        // if (this.red || this.green || this.blue) {
-        //     black = Math.min(cyan, Math.min(magenta, yellow));
-        //     cyan = (cyan - black) / (1 - black);
-        //     magenta = (magenta - black) / (1 - black);
-        //     yellow = (yellow - black) / (1 - black);
-        // } else {
-        //     black = 1;
-        // }
-
-        // return Math.round(cyan * 255) + ', ' + Math.round(magenta * 255) + ', ' + Math.round(yellow * 255) + ', ' + Math.round(black + 254);
-        // var result = new CMYK(0, 0, 0, 0);
-
-        let r = this.red / 255;
-        let g = this.green / 255;
-        let b = this.blue / 255;
-
-        let k = Math.min(1 - r, 1 - g, 1 - b);
-        let c = (1 - r - k) / (1 - k);
-        let m = (1 - g - k) / (1 - k);
-        let y = (1 - b - k) / (1 - k);
+        let k: number = Math.min(1 - r, 1 - g, 1 - b);
+        let c: number = (1 - r - k) / (1 - k);
+        let m: number = (1 - g - k) / (1 - k);
+        let y: number = (1 - b - k) / (1 - k);
 
         return Math.round(c * 100) + ', ' + Math.round(m * 100) + ', ' + Math.round(y * 100) + ', ' + Math.round(k * 100);
     }
