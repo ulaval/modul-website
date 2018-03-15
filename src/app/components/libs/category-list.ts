14d43ebd3ee35646864459dba16250bb046ef96b
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2018-03-15 10:20:42 -0400

    Add status tag in category-list

--- src/app/components/libs/category-list.ts
+++ src/app/components/libs/category-list.ts
@@ -1,7 +1,7 @@
 import Component from 'vue-class-component';
 import WithRender from './category-list.html?style=./category-list.scss';
 import { ModulWebsite } from '../modul-website';
-import Meta, { ComponentMetaEx } from '../../meta/meta-all';
+import Meta, { ComponentMetaEx, ModulComponentStatus } from '../../meta/meta-all';
 import { GET_CATEGORY } from '@/app/store/modules/components/getters';
 
 @WithRender
@@ -21,5 +21,9 @@ export class CategoryList extends ModulWebsite {
 
     private onComponentClick(tag: string): void {
         this.$router.push(this.$routerIndex.for(tag));
+    }
+
+    private isProduction(status): boolean {
+        return status === ModulComponentStatus.Production;
     }
 }
