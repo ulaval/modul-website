432378f31f447c7e685f5e8bbe0b823ac1ed3f5f
Charles Maheu <CHMAH33@ulaval.ca>
2018-12-17 12:17:51 -0500

    fixed layout

--- src/pages/standards/standards.ts
+++ src/pages/standards/standards.ts
@@ -1,16 +1,31 @@
 
+import { MediaQueries, MediaQueriesMixin } from '@ulaval/modul-components/dist/mixins/media-queries/media-queries';
 import { ModulVue } from '@ulaval/modul-components/dist/utils/vue/vue';
 import Component from 'vue-class-component';
+import { Watch } from 'vue-property-decorator';
 import WithRender from './standards.html?style=./standards.scss';
 
 @WithRender
-@Component
+@Component({
+    mixins: [MediaQueries]
+})
 export class MWStandardsPage extends ModulVue {
 
     showMenu = false;
 
     mounted() {
-        this.showMenu = true;
+        this.isMqMinMChanged(this.as<MediaQueriesMixin>().isMqMinM);
+    }
+
+    @Watch('isMqMinM')
+    private isMqMinMChanged(value): void {
+        this.showMenu = value;
+    }
+
+    onSideMenuSelection() {
+        if (this.showMenu && this.as<MediaQueriesMixin>().isMqMaxM) {
+            this.showMenu = false;
+        }
     }
 
     toggleMenu() {
